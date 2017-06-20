198af997150b425b7c9b97fb07fdfa3c83d1a143
Jared McQueen <jaredmcqueen@gmail.com>
2017-06-19 23:29:49 -0400

    updated public_suffix version to 2.0.5

--- lib/logstash/filters/tld.rb
+++ lib/logstash/filters/tld.rb
@@ -2,43 +2,9 @@
 require "logstash/filters/base"
 require "logstash/namespace"
 
-# This filter is a domain name parser based on the https://publicsuffix.org/[Public Suffix List]
-# for more information on this ruby parser, see https://github.com/weppos/publicsuffix-ruby
-#
-# by default, parsing "example.s3.amazonaws.com" would yield:
-#
-# [source,json]
-# ----------------------------------
-# "tld": {
-#   "trd": "example.s3",
-#   "domain": "amazonaws.com",
-#   "subdomain": "example.s3.amazonaws.com",
-#   "sld": "amazonaws",
-#   "tld": "com"
-# }
-# ----------------------------------
-#
-# setting `ignore_private => false` enables private (non-ICANN) domain parsing:
-#
-# [source,json]
-# ----------------------------------
-# "tld": {
-#   "domain": "example.s3.amazonaws.com",
-#   "sld": "example",
-#   "tld": "s3.amazonaws.com"
-# }
-# ----------------------------------
+
 class LogStash::Filters::Tld < LogStash::Filters::Base
 
-  # Setting the config_name here is required. This is how you
-  # configure this filter from your Logstash config.
-  #
-  # filter {
-  #   example {
-  #     message => "My message..."
-  #   }
-  # }
-  #
   config_name "tld"
 
   # The source field to parse
@@ -54,7 +20,6 @@ class LogStash::Filters::Tld < LogStash::Filters::Base
   def register
     # Add instance variables
     require 'public_suffix'
-    PublicSuffix::List.private_domains = false
   end # def register
 
   public
--- spec/filters/tld_spec.rb
+++ spec/filters/tld_spec.rb
@@ -10,21 +10,6 @@ describe LogStash::Filters::Tld do
       }
     CONFIG
 
-#{
-#       "message" => "google.com",
-#      "@version" => "1",
-#    "@timestamp" => "2015-01-22T17:33:19.669Z",
-#          "host" => "homer",
-#      "sequence" => 0,
-#           "tld" => {
-#              "tld" => "com",
-#              "sld" => "google",
-#              "trd" => nil,
-#           "domain" => "google.com",
-#        "subdomain" => nil
-#    }
-
-
     sample("message" => "google.com") do
       insist { subject.get("tld")["tld"] } == "com"
       insist { subject.get("tld")["sld"] } == "google"
